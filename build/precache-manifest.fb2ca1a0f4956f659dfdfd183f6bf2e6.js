9ccdf764bf4663c3afa9a6b0971ffc302cb74167
ahmedsoumri01 <95105227+ahmedsoumri01@users.noreply.github.com>
2022-09-15 18:08:28 +0100

    Add files via upload

---
+++ build/precache-manifest.fb2ca1a0f4956f659dfdfd183f6bf2e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28fa55888366db13d3e226a0558605b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "972ae6c161d830c249cd",
+    "url": "/static/js/main.1297f49d.chunk.js"
+  },
+  {
+    "revision": "10e117344dea3db99e26",
+    "url": "/static/js/runtime-main.b5766938.js"
+  }
+]);
